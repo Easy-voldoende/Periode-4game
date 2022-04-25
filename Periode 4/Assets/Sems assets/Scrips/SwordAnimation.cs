67ba05bbc8d447486080b64bf6eaced53925da0f
Semmieeeh <99257556+Semmieeeh@users.noreply.github.com>
2022-04-25 23:06:36 +0200

    Sword attack speed fix

--- Periode 4/Assets/Sems assets/Scrips/SwordAnimation.cs
+++ Periode 4/Assets/Sems assets/Scrips/SwordAnimation.cs
@@ -10,6 +10,7 @@ public class SwordAnimation : MonoBehaviour
     public ShootScript shootScript;
     public float weaponCycle;
     
+    
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
--- Periode 4/Assets/Sems assets/Scrips/SwordDamage.cs
+++ Periode 4/Assets/Sems assets/Scrips/SwordDamage.cs
@@ -6,30 +6,40 @@ public class SwordDamage : MonoBehaviour
 {
     public Vector3 v;
     public float range;
-    public float attackCooldown = 1f;
+    public float attackCooldown = 2f;
     public float damage = 30;
-    public bool attack = true;
+    
+    public bool canAttack;
 
     
     public Camera fpsCamera;
     public SwordDamage swordDamage;
+    public SwordAnimation swordAnimation;
 
     public void Update()
     {
        
-        
             if (Input.GetButtonDown("Fire1"))
             {
+               
+                
+               if(canAttack == true)
+               {
                 Shoot();
+               }
+                
 
             }
+        
+        
+           
         
 
     }
     public void Start()
     {
         range = 10f;
-        
+        canAttack = true;
     }
 
 
@@ -38,27 +48,35 @@ public class SwordDamage : MonoBehaviour
         
         RaycastHit hit;
         
-        attack = false;
+        
+          
+          
             if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
             {
                 Enemy enemy = hit.transform.GetComponent<Enemy>();
                 {
                     enemy.TakeDamage(damage);
                     Debug.Log("Slash!");
-                    
+                    canAttack = false;
+                    StartCoroutine(ResetAttackCoolDown());
                 }
             }
+          
         
        
 
         
-     
+      
 
     }
-    
 
 
 
+    IEnumerator ResetAttackCoolDown()
+    {
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+    }
 
 
 }
