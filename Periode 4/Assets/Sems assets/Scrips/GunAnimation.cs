b3de8fcd2f444c8213327aade6edfcd0ee451003
Semmieeeh <99257556+Semmieeeh@users.noreply.github.com>
2022-05-12 13:38:59 +0200

    ff kijken
    FF kijken

--- Periode 4/Assets/Sems assets/Scrips/GunAnimation.cs
+++ Periode 4/Assets/Sems assets/Scrips/GunAnimation.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwordAnimation : MonoBehaviour
+public class GunAnimation : MonoBehaviour
 {
-    public GameObject sword, gun;
+    public GameObject gun;
     public bool canAttack = true;
     public float attackCooldown = 1.0f;
     public ShootScript shootScript;
     public float weaponCycle;
-    
-    
+
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if(canAttack)
+            if (canAttack)
             {
                 SwordAttack();
             }
@@ -25,18 +25,18 @@ public class SwordAnimation : MonoBehaviour
     public void SwordAttack()
     {
         canAttack = false;
-        Animator anim = sword.GetComponent<Animator>();
-        anim.SetTrigger("Slash");
+        Animator anim = gun.GetComponent<Animator>();
+        anim.SetTrigger("Shot");
         StartCoroutine(ResetAttackCooldown());
     }
     IEnumerator ResetAttackCooldown()
     {
         yield return new WaitForSeconds(attackCooldown);
         canAttack = true;
-        
+
     }
 
-   
+
     public void Start()
     {
         weaponCycle = 0f;
