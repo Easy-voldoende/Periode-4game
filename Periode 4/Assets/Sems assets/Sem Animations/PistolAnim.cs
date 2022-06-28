6445afa92f62cf295b015aae83dffbbf56b7472b
Semmieeeh <99257556+Semmieeeh@users.noreply.github.com>
2022-06-28 15:30:44 +0200

    gun animation begin

---
+++ Periode 4/Assets/Sems assets/Sem Animations/PistolAnim.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolAnim : MonoBehaviour
+{
+    public GameObject sword, gun;
+    public bool canAttack = true;
+    public float attackCooldown = 0.1f;
+    public ShootScript1 shootScript;
+    public float weaponCycle;
+    public WeaponSwitch weaponswitch;
+    public int attackState;
+    public float attackTime;
+
+
+
+
+    void Update()
+    {
+
+        if (attackState >1) 
+        {
+            attackState = 0;
+            canAttack = true;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canAttack)
+            {
+                GunShot();
+
+
+
+            }
+        }
+        if (attackTime > 10)
+        {
+            attackTime = 10;
+        }
+        attackTime -= 1 * Time.deltaTime * 10;
+        if (Input.GetMouseButtonDown(0))
+        {
+            attackTime += 8;
+        }
+
+        if (attackTime < 5)
+        {
+            attackState = 0;
+        }
+
+        if (attackTime < 0)
+        {
+            attackTime = 0;
+
+        }
+        if (attackTime < 1)
+        {
+
+            if (attackState > 1)
+            {
+                attackState = 0;
+            }
+        }
+        if (attackState == 0)
+        {
+            attackCooldown = 0;
+        }
+        if(attackTime > 1)
+        {
+            canAttack = false;
+        }
+        if(attackTime < 1)
+        {
+            canAttack = true;
+        }
+        Animator anim = sword.GetComponent<Animator>();
+        anim.SetInteger("AttackState", attackState);
+    }
+
+
+    public void GunShot()
+    {
+
+        Animator anim = gun.GetComponent<Animator>();
+        anim.SetInteger("Fire", attackState);
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            attackState += 1;
+
+
+        }
+
+        if (attackState > 1)
+        {
+            attackState = 0;
+        }
+
+
+
+
+    }
+
+
+
+    IEnumerator ResetAttackCooldown()
+    {
+
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+        attackState = 0;
+
+
+
+
+    }
+
+
+    public void Start()
+    {
+        weaponCycle = 0f;
+        weaponswitch = GameObject.Find("Player").GetComponent<WeaponSwitch>();
+
+    }
+}
